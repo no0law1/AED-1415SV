4bd8c7be7a83af71332382e9d7aaa7e767dfd90d
no0law1 <nuno.m.reis@netcabo.pt>
2015-05-20 00:01:53 +0100

    ListUtils complete.

--- main/java/serie2/ListUtils.java
+++ main/java/serie2/ListUtils.java
@@ -10,68 +10,79 @@ import java.util.Comparator;
 public class ListUtils {
 
     public static <E> E getKBiggest(Node<E> list, int k, Comparator<E> cmp) {
-        //TODO: Don't understand the concept of the problem
-        if (k > 0) {
-            int n = 0;
-            Node<E> result = list;
-            while (list != null && n<k) {
-                if (cmp.compare(list.value, result.value) > 0) {
-                    result.value = list.value;
-                }
-                list = list.next;
-                n++;
-            }
-            return (n==k) ? result.value : null;
+        if ( list == null || k <= 0 ){
+            return null;
         }
-        return null;
+        int n = 1;
+        Node<E> result = list;
+        Node<E> lastNode = list;
+        while (n++ < k){
+            lastNode = lastNode.next;
+            if(lastNode.next == null){
+                break;
+            }
+        }
+        if(n<k){
+            return null;
+        }
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            result = result.next;
+        }
+
+        return result.value;
     }
 
     public static <E> Node<E> occurAtLeastKTimes(Node<E>[] lists, Comparator<E> cmp, int k) {
+
+        Comparator<Node<E>> newCmp = (i1, i2) -> {
+            if (i1 == null) return Integer.MAX_VALUE;
+            if (i2 == null) return Integer.MIN_VALUE;
+            return (cmp.compare(i1.value, i2.value));
+        };
+
         Node<E> result = new Node<>();
         result.next = result;
         result.previous = result;
 
-        Node<E> aux = result;
-        Node<E> toBeSearched = lists[0];
-
-        int numberOfTimes = 1;
+        int numberOfTimes = 0;
         int size = lists.length - 1;
 
-        Heap.sort(lists, 0, size, (i1, i2) -> {
-            if (i1 == null) return Integer.MAX_VALUE;
-            if (i2 == null) return Integer.MIN_VALUE;
-            return (cmp.compare(i1.value, i2.value));
-        });
+        Heap.minHeapify(lists, 0, size, newCmp);
 
-        while (lists[0] != null){
-            Node<E> recurrentSearch = lists[0];
-            if(cmp.compare(recurrentSearch.value, toBeSearched.value) == 0) {
+        Node<E> toBeSearched = lists[0];
+
+        while(true){
+            Node<E> max = lists[0];
+
+            if(newCmp.compare(max, toBeSearched) == 0){
                 numberOfTimes++;
-                lists[0] = lists[0].next;
-            } else{
+            } else {
                 if(numberOfTimes >= k){
-                    Node<E> onlyOnce = new Node<>(toBeSearched.value);
-                    onlyOnce.next = aux.next;
-                    onlyOnce.previous = aux;
-                    aux.next.previous = onlyOnce;
-                    aux.next = onlyOnce;
-
-                    aux = aux.next;
+                    addToList(result, toBeSearched);
                 }
                 numberOfTimes = 1;
-                toBeSearched = lists[0];
+                toBeSearched = max;
+                if(lists[0]==null){
+                    break;
+                }
             }
-
+            lists[0] = lists[0].next;
             if(lists[0] == null){
-                lists[0] = lists[size--];
+                lists[0] = lists[size];
+                lists[size--] = null;
             }
+            Heap.minHeapify(lists, 0, size, newCmp);
 
-            Heap.sort(lists, 0, size, (i1, i2) -> {
-                if (i1 == null) return Integer.MAX_VALUE;
-                if (i2 == null) return Integer.MIN_VALUE;
-                return (cmp.compare(i1.value, i2.value));
-            });
         }
+
         return result;
+    }
+
+    public static <E> void addToList(Node<E> list, Node<E> node){
+        node.next = list;
+        node.previous = list.previous;
+        list.previous.next = node;
+        list.previous = node;
     }
 }
