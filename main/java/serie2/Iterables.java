31f6fbd5483ede09cd9a9a4b6bcec498e8abfc47
no0law1 <nuno.m.reis@netcabo.pt>
2015-05-20 10:03:10 +0100

    getKeys complete.

--- main/java/serie2/Iterables.java
+++ main/java/serie2/Iterables.java
@@ -46,6 +46,41 @@ public class Iterables {
     }
 
     public static<K,V> Iterable<K> getKeys(HashNode<K,V>[] hashMap){
-        return null;
+        return () -> new Iterator<K>() {
+            K curr = null;
+            int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                if(curr != null){
+                    return true;
+                }
+                while (i < hashMap.length) {
+                    if(hashMap[i] == null){
+                        i++;
+                    } else {
+                        curr = hashMap[i].key;
+                        hashMap[i] = hashMap[i].next;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public K next() {
+                if(hasNext()){
+                    K aux = curr;
+                    curr = null;
+                    return aux;
+                }
+                throw new NoSuchElementException("getValuesBetween:Iterator - no such element");
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("getValuesBetween:Iterator - remove not supported");
+            }
+        };
     }
 }
